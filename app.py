c4f198ec786a7e36a317957dfc692103ff90f1fa
marchoag <marc@marchoag.com>
2025-06-10 14:57:19 -0700

    Fix port configuration for deployment

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def api_status():
         })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001) 
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=False, host='0.0.0.0', port=port) 
